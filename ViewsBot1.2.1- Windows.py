20cc06f7b2a4108078cefb45e7b4737885b3f9d7
Allacks <66631379+Allacks@users.noreply.github.com>
2020-07-06 17:38:08 +0200

    Add files via upload

---
+++ ViewsBot1.2.1- Windows.py
@@ -0,0 +1,36 @@
+import webbrowser   #You may have to install these modules
+import time
+import os
+import threading
+
+num = 1
+Browser = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+
+
+def watch():
+    webbrowser.get(Browser).open('https://www.youtube.com/watch?v=WXxV9g7lsFE') #Insert Link here between those keys ''
+
+
+
+def threadA():
+    threading.Thread(target=watch).start()
+
+
+
+def LoopIt():
+   threadA()
+   time.sleep(25)
+   os.system("taskkill /im chrome.exe /f")
+   time.sleep(2)
+   threadA()
+   time.sleep(21)
+   os.system("taskkill /im chrome.exe /f")
+   time.sleep(3)
+   threadA()
+   time.sleep(23)
+   os.system("taskkill /im chrome.exe /f")
+   time.sleep(2)
+   LoopIt()
+
+
+LoopIt()
